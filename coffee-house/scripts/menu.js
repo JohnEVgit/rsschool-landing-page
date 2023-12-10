2ab93030a8e9b4ad7aa032cf4583deb4af01a5bf
Yauheni Paloika <john13ev@gmail.com>
2023-12-10 20:03:12 +0400

    fix: menu grid and popup logic

--- coffee-house/scripts/menu.js
+++ coffee-house/scripts/menu.js
@@ -1,131 +1,13 @@
-const menuList = {
-    coffee: [
-        {
-            id: 1,
-            title: 'Irish coffee',
-            description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
-            price: 7,
-        },
-        {
-            id: 2,
-            title: 'Kahlua coffee',
-            description: 'Classic coffee with milk and Kahlua liqueur under a cap of frothed milk',
-            price: 7,
-        },
-        {
-            id: 3,
-            title: 'Honey raf',
-            description: 'Espresso with frothed milk, cream and aromatic honey',
-            price: 5.5,
-        },
-        {
-            id: 4,
-            title: 'Ice cappuccino',
-            description: 'Cappuccino with soft thick foam in summer version with ice',
-            price: 5,
-        },
-        {
-            id: 5,
-            title: 'Espresso',
-            description: 'Classic black coffee',
-            price: 4.5,
-        },
-        {
-            id: 6,
-            title: 'Latte',
-            description: 'Espresso coffee with the addition of steamed milk and dense milk foam',
-            price: 5.5,
-        },
-        {
-            id: 7,
-            title: 'Latte macchiato',
-            description: 'Espresso with frothed milk and chocolate',
-            price: 5.5,
-        },
-        {
-            id: 8,
-            title: 'Coffee with cognac',
-            description: 'Fragrant black coffee with cognac and whipped cream',
-            price: 6.5,
-        },
-    ],
-    tea: [
-        {
-            id: 1,
-            title: 'Moroccan',
-            description: 'Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint',
-            price: 4.5,
-        },
-        {
-            id: 2,
-            title: 'Ginger',
-            description: 'Original black tea with fresh ginger, lemon and honey',
-            price: 5,
-        },
-        {
-            id: 3,
-            title: 'Cranberry',
-            description: 'Invigorating black tea with cranberry and honey',
-            price: 5,
-        },
-        {
-            id: 4,
-            title: 'Sea buckthorn',
-            description: 'Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon',
-            price: 5.5,
-        },
-    ],
-    dessert: [
-        {
-            id: 1,
-            title: 'Marble cheesecake',
-            description: 'Philadelphia cheese with lemon zest on a light sponge cake and red currant jam',
-            price: 3.5,
-        },
-        {
-            id: 2,
-            title: 'Red velvet',
-            description: 'Layer cake with cream cheese frosting',
-            price: 4,
-        },
-        {
-            id: 3,
-            title: 'Cheesecakes',
-            description: 'Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar',
-            price: 4.5,
-        },
-        {
-            id: 4,
-            title: 'Creme brulee',
-            description: 'Delicate creamy dessert in a caramel basket with wild berries',
-            price: 4,
-        },
-        {
-            id: 5,
-            title: 'Pancakes',
-            description: 'Tender pancakes with strawberry jam and fresh strawberries',
-            price: 4.5,
-        },
-        {
-            id: 6,
-            title: 'Honey cake',
-            description: 'Classic honey cake with delicate custard',
-            price: 4.5,
-        },
-        {
-            id: 7,
-            title: 'Chocolate cake',
-            description: 'Cake with hot chocolate filling and nuts with dried apricots',
-            price: 5.5,
-        },
-        {
-            id: 8,
-            title: 'Black forest',
-            description: 'A combination of thin sponge cake with cherry jam and light chocolate mousse',
-            price: 6.5,
-        },
-    ],
-};
+let productsObj;
+
+fetch('./json/products.json')
+    .then(response => response.json())
+    .then(data => {
+        productsObj = data;
+
+        addMenuLayout();
+    })
+    .catch(error => console.log(error));
 
 const menuGridElem = bodyElem.querySelector('.menu-grid-js');
 const menuMoreButtonElem = bodyElem.querySelector('.menu-more-button-js');
@@ -144,13 +26,15 @@ let menuGridStartPosition = 0;
 let menuGridCategory = 'coffee';
 let menuGridLayout = '';
 
+let menuCategoryList = [];
+
 const menuSizeTabStartElem = bodyElem.querySelector('#menu-tab-coffee.menu-tab-js');
 if (!menuSizeTabStartElem.checked) {
     menuSizeTabStartElem.checked = true;
 }
 
 const showMenuMoreButton = () => {
-    if (menuGridStartPosition < menuList[menuGridCategory].length) {
+    if (menuGridStartPosition < menuCategoryList.length) {
         menuMoreButtonElem.classList.add('menu__more--is-active');
     } else {
         menuMoreButtonElem.classList.remove('menu__more--is-active');
@@ -158,22 +42,21 @@ const showMenuMoreButton = () => {
 };
 
 const createMenuLayout = () => {
-    const menuCategoryList = menuList[menuGridCategory];
-
     for (let i = menuGridStartPosition; i < menuGridCount * menuGridPage && i !== menuCategoryList.length; i++) {
         const thisEl = menuCategoryList[i];
+
         menuGridLayout += `
-        <div class="menu__item menu-item" data-id="${thisEl.id}" tabindex="0">
+        <div class="menu__item menu-item" data-id="${i}" tabindex="0">
             <picture class="menu-item__picture">
-                <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}.avif 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}-2x.avif 2x" type="image/avif">
-                <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}.webp 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}-2x.webp 2x" type="image/webp">
-                <img class="menu-item__img" src="images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}.jpg" alt="${thisEl.title}" width="340" height="340"
-                    srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}.jpg 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${thisEl.id}-2x.jpg 2x">
+                <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}.avif 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}-2x.avif 2x" type="image/avif">
+                <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}.webp 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}-2x.webp 2x" type="image/webp">
+                <img class="menu-item__img" src="images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}.jpg" alt="${thisEl.name}" width="340" height="340"
+                    srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}.jpg 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${i + 1}-2x.jpg 2x">
             </picture>
             <div class="menu-item__info">
-                <h3 class="menu-item__title">${thisEl.title}</h3>
+                <h3 class="menu-item__title">${thisEl.name}</h3>
                 <p class="menu-item__text">${thisEl.description}</p>
-                <p class="menu-item__price">$${thisEl.price.toFixed(2)}</p>
+                <p class="menu-item__price">$${thisEl.price}</p>
             </div>
         </div>`;
     }
@@ -188,6 +71,8 @@ const resetMenuLayout = () => {
 };
 
 const addMenuLayout = () => {
+    menuCategoryList = productsObj.filter(elem => elem.category === menuGridCategory);
+
     createMenuLayout();
 
     menuGridElem.innerHTML += menuGridLayout;
@@ -196,8 +81,6 @@ const addMenuLayout = () => {
     menuGridStartPosition = getMenuGridStartPosition();
     showMenuMoreButton();
 };
-
-addMenuLayout();
 
 menuMoreButtonElem.addEventListener('click', () => {
     menuGridPage += 1;
@@ -230,7 +113,7 @@ const showMenuPopup = (e) => {
     const thisElem = e.target.closest('.menu-item');
 
     if (thisElem) {
-        createPopup(thisElem.dataset.id);
+        createPopup(+thisElem.dataset.id);
         popupElem.showModal();
         bodyElem.classList.add('page__body--is-fixed');
     }
@@ -247,82 +130,140 @@ menuGridElem.addEventListener("keydown", (e) => {
 });
 
 const popupElem = bodyElem.querySelector('.popup-js');
-const popupCloseButtonElem = bodyElem.querySelector('.close-popup-js');
-
-const popupPictureElem = bodyElem.querySelector('.popup-picture-js');
-const popupTitleElem = bodyElem.querySelector('.popup-title-js');
-const popupDescriptionElem = bodyElem.querySelector('.popup-description-js');
-const popupPriceElem = bodyElem.querySelector('.popup-price-js');
+let popupCloseButtonElem;
+let popupPriceElem;
 
 let popupPrice = 0;
 let popupSizePrice = 0;
 let popupAdditivesPrice = 0;
+let popupLayout = '';
 
 const calcPrice = (price) => {
     return (price + popupSizePrice + popupAdditivesPrice).toFixed(2);
 };
 
-const createPopupImage = (id) => {
-    return `<source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id}.avif 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id}-2x.avif 2x" type="image/avif">
-    <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id}.webp 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id}-2x.webp 2x" type="image/webp">
-    <img class="popup__img" src="images/menu/${menuGridCategory}/${menuGridCategory}-${id}.jpg" alt="Irish coffee" width="340" height="340"
-      srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id}.jpg 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id}-2x.jpg 2x">`;
+const createPopupSizeTabsLayout = (id) => {
+    let popupSizeTabsLayout = '';
+    const popupSizeTabsObj = menuCategoryList[id].sizes;
+    let i = 0;
+
+    for (tab in popupSizeTabsObj) {
+        popupSizeTabsLayout += `<li class="popup__tab tab">
+        <input class="tab__input visually-hidden popup-tab-size-js" type="radio" name="size-tab" id="size-tab-${tab}"
+            data-size-price="${popupSizeTabsObj[tab]['add-price']}"${i === 0 ? ' checked' : ''}>
+            <label class="tab__label" data-tab="${tab}" for="size-tab-${tab}">${popupSizeTabsObj[tab].size}</label>
+        </li>`;
+        i++;
+    };
+
+    return popupSizeTabsLayout;
+}
+
+const createPopupAdditivesTabsLayout = (id) => {
+    let popupAdditivesTabsLayout = '';
+    const popupSizeTabsArr = menuCategoryList[id].additives;
+
+    for (let i = 0; i < popupSizeTabsArr.length; i++) {
+        popupAdditivesTabsLayout += `<li class="popup__tab tab">
+            <input class="tab__input visually-hidden popup-tab-additives-js" type="checkbox" name="additives-tab-${i + 1}"
+                id="additives-tab-${i + 1}" data-additives-price="${popupSizeTabsArr[i]['add-price']}">
+            <label class="tab__label" data-tab="${i + 1}" for="additives-tab-${i + 1}">${popupSizeTabsArr[i].name}</label>
+        </li>`;
+    };
+
+    return popupAdditivesTabsLayout;
+}
+
+const createPopupLayout = (id) => {
+    popupLayout = `<div class="popup__wrapper">
+    <picture class="popup__picture popup-picture-js">
+        <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}.avif 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}-2x.avif 2x" type="image/avif">
+        <source srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}.webp 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}-2x.webp 2x" type="image/webp">
+        <img class="popup__img" src="images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}.jpg" alt="Irish coffee" width="340" height="340"
+        srcset="images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}.jpg 1x, images/menu/${menuGridCategory}/${menuGridCategory}-${id + 1}-2x.jpg 2x">
+    </picture>
+    <div class="popup__info">
+        <div class="popup__text">
+            <h3 class="popup__title popup-title-js">${menuCategoryList[id].name}</h3>
+            <p class="popup__description popup-description-js">${menuCategoryList[id].description}</p>
+        </div>
+        <div class="popup__options">
+            <p class="popup__label">Size</p>
+            <ul class="popup__tabs">${createPopupSizeTabsLayout(id)}</ul>
+        </div>
+        <div class="popup__options">
+            <p class="popup__label">Additives</p>
+            <ul class="popup__tabs">${createPopupAdditivesTabsLayout(id)}</ul>
+        </div>
+        <p class="popup__price">
+            <span>Total:</span>
+            <span class="popup-price-js">$${menuCategoryList[id].price}</span>
+        </p>
+        <div class="popup__alert popup-alert">
+            <svg class="popup-alert__icon" width="16" height="16" aria-hidden="true">
+                <use href="images/sprite.svg#alert" />
+            </svg>
+            <p class="popup-alert__text">The cost is not final. Download our mobile app to see the final price and place
+                your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
+        </div>
+        <button class="popup__close button button--theme-secondary close-popup-js" type="button">Close</button>
+    </div>
+</div>`};
+
+const popupCloseButtonHandle = () => {
+    popupElem.close();
 };
 
-const createPopup = (id) => {
-    popupPictureElem.innerHTML = createPopupImage(id);
-    popupTitleElem.textContent = menuList[menuGridCategory][id - 1].title;
-    popupDescriptionElem.textContent = menuList[menuGridCategory][id - 1].description;
+const popupTabSizeHandle = (e) => {
+    thisElem = e.target;
+    popupSizePrice = +thisElem.dataset.sizePrice;
+    popupPriceElem.textContent = `$${calcPrice(popupPrice)}`;
+};
 
-    popupPrice = menuList[menuGridCategory][id - 1].price;
+const popupTabAdditivesHandle = (e) => {
+    thisElem = e.target;
+
+    if (thisElem.checked) {
+        popupAdditivesPrice += +thisElem.dataset.additivesPrice;
+    } else {
+        popupAdditivesPrice -= +thisElem.dataset.additivesPrice;
+    }
+
     popupPriceElem.textContent = `$${calcPrice(popupPrice)}`;
 };
 
 const resetPopupPrice = () => {
     popupSizePrice = 0;
     popupAdditivesPrice = 0;
-
-    bodyElem.querySelector('#size-tab-s.popup-tab-size-js').checked = true;
-
-    bodyElem.querySelectorAll(".popup-tab-addiives-js").forEach((elem) => {
-        elem.checked = false;
-    });
 }
 
-popupCloseButtonElem.addEventListener("click", () => {
-    popupElem.close();
-});
+const createPopup = (id) => {
+    createPopupLayout(id);
+    popupElem.innerHTML = popupLayout;
+
+    popupPrice = +menuCategoryList[id].price;
+
+    popupCloseButtonElem = bodyElem.querySelector('.close-popup-js');
+    popupPriceElem = bodyElem.querySelector('.popup-price-js');
+
+    popupCloseButtonElem.addEventListener("click", popupCloseButtonHandle);
+
+    popupElem.querySelectorAll(".popup-tab-size-js").forEach((elem) => {
+        elem.addEventListener('change', popupTabSizeHandle);
+    });
+
+    popupElem.querySelectorAll(".popup-tab-additives-js").forEach((elem) => {
+        elem.addEventListener('change', popupTabAdditivesHandle);
+    });
+};
 
 popupElem.addEventListener("close", () => {
     bodyElem.classList.remove('page__body--is-fixed');
-
     resetPopupPrice();
 });
 
 popupElem.addEventListener("click", (e) => {
     if (!e.target.closest('.popup__wrapper')) {
-        popupCloseButtonElem.click();
+        popupElem.close();
     }
-});
-
-bodyElem.querySelectorAll(".popup-tab-size-js").forEach((elem) => {
-    elem.addEventListener('change', (e) => {
-        thisElem = e.target;
-        popupSizePrice = +thisElem.dataset.sizePrice;
-        popupPriceElem.textContent = `$${calcPrice(popupPrice)}`;
-    });
-});
-
-bodyElem.querySelectorAll(".popup-tab-addiives-js").forEach((elem) => {
-    elem.addEventListener('change', (e) => {
-        thisElem = e.target;
-
-        if (thisElem.checked) {
-            popupAdditivesPrice += +thisElem.dataset.additivesPrice;
-        } else {
-            popupAdditivesPrice -= +thisElem.dataset.additivesPrice;
-        }
-
-        popupPriceElem.textContent = `$${calcPrice(popupPrice)}`;
-    });
 });
